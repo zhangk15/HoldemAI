62539e2d5421ab0980f0432bb88747017833822c
henrylee20 <443786959@qq.com>
2015-05-25 01:30:43 +0800

    comunication model

---
+++ com_model.py
@@ -0,0 +1,131 @@
+#-*- coding: utf-8 -*-
+import socket
+import sys
+import com_handle
+
+class game_link:
+	def __init__(self, target_addr, self_addr):
+		self.__target_addr = target_addr
+		self.__self_addr = self_addr
+		self.__sock = None
+	
+	def __del__(self):
+		#pass
+		if self.__sock != None:
+			self.__sock.close()
+	
+	def __send(self, msg):
+		#print(bytes(msg, encoding = 'ascii'))
+		try:
+			self.__sock.send(bytes(msg)) #, encoding = 'ascii'))
+		except socket.error as msg:
+			print("cannot send!")
+			print(msg)
+			return
+
+	def __recv(self):
+		try:
+			buf = self.__sock.recv(2048)
+		except socket.error as msg:
+			print("cannot recv!")
+			print(msg)
+			return ""
+
+		return str(buf) #, encoding = 'ascii')
+
+	def link_start(self):
+		self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.__sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+		try:
+			self.__sock.bind(self.__self_addr)
+		except socket.error as meg:
+			print(msg)
+			print("Bind Error!!!")
+			self.__sock.close()
+			self.__sock = None
+		try:
+			self.__sock.connect(self.__target_addr)
+		except socket.error as msg:
+			print(msg)
+			print("Connection Error!!!")
+			self.__sock.close()
+			self.__sock = None
+
+		if self.__sock is None:
+			print("cannot connect")
+			return False
+		return True
+
+		
+	def msg_reg(self, pid, pname):
+		self.__send('reg: ' + str(pid) + ' ' + pname + ' \n')
+
+	def msg_action(self, action):
+		self.__send('check ' + action + '\n')
+	
+	def get_msg(self):
+		return self.__recv()
+
+
+def main(argv):
+	for arg in argv:
+		print(arg)
+
+	try:
+		target_addr = (argv[1], int(argv[2]))
+		self_addr = (argv[3], int(argv[4]))
+	except IndexError:
+		print("Wrong arg!")
+		return
+
+	game_conn = game_link(target_addr, self_addr)
+	if not game_conn.link_start():
+		print("Can't connect")
+		return
+	game_conn.msg_reg(argv[5], 'HL')
+	
+	raw_msg = game_conn.get_msg()
+	print("seat:")
+	print(com_handle.patten_seat.findall(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	print("blind:")
+	print(com_handle.get_bet(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	print("hold cards:")
+	print(com_handle.get_cards(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	print("users action:")
+	print(com_handle.get_userinfo(raw_msg))
+	game_conn.msg_action('fold')
+
+	raw_msg = game_conn.get_msg()
+	print("flop cards:")
+	print(com_handle.get_cards(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	print("turn cards:")
+	print(com_handle.get_card(raw_msg))
+		
+	raw_msg = game_conn.get_msg()
+	print("river cards:")
+	print(com_handle.get_cards(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	print("shutdown:")
+	print(com_handle.get_cards(raw_msg))
+	print(com_handle.patten_shutdown_rank.findall(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	print("win:")
+	print(com_handle.get_bet(raw_msg))
+
+	raw_msg = game_conn.get_msg()
+	if raw_msg == "game-over \n":
+		print("Game over!")
+
+
+if __name__ == '__main__':
+	main(sys.argv)
